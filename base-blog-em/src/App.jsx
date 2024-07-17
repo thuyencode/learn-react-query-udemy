a03b9a29ec31c7434d45b5fafe6c0988decfe6b0
Thuyen Code <thuyencode@proton.me>
2024-07-17 10:54:12 +0700

    React Query Dev Tools

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './App.css'
 import { Posts } from './Posts'
 
@@ -12,6 +13,8 @@ function App() {
         <h1>Blog &apos;em Ipsum</h1>
         <Posts />
       </div>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
