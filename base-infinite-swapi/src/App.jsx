7fbb2f05ff177067e0c806fd18d6bfa9e1581dee
Thuyen Code <thuyencode@proton.me>
2024-07-19 09:26:17 +0700

    Code Quiz! Set up Infinite SWAPI for React Query

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,15 +1,22 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import './App.css'
+import { InfinitePeople } from './people/InfinitePeople'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,20 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller'
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/'
+
+/**
+ * The general fetcher
+ *
+ * @async
+ * @param {string} url
+ * @returns {Promise<unknown>}
+ */
 const fetchUrl = async (url) => {
-  const response = await fetch(url);
-  return response.json();
-};
+  const response = await fetch(url)
+  return response.json()
+}
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  return <InfiniteScroll />
 }
