028e73a9a6430f1376cddbd09b36af4b5cf5a7a1
Thuyen Code <thuyencode@proton.me>
2024-07-19 09:19:06 +0700

    Update React version to 18.3 and use ES2021 standard in ESLint configs

--- completed-apps/blog-em-ipsum/.eslintrc.cjs
+++ completed-apps/blog-em-ipsum/.eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:@tanstack/eslint-plugin-query/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.3' } },
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
     ],
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
-  },
-};
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn'
+  }
+}
