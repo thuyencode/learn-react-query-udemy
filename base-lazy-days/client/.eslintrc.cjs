4d32d9ccfcaca534b7aa14f91fff30da6e59685e
Thuyen Code <thuyencode@proton.me>
2024-07-20 09:40:46 +0700

    Install React Query

--- base-lazy-days/client/.eslintrc.cjs
+++ base-lazy-days/client/.eslintrc.cjs
@@ -19,8 +19,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:vitest/recommended',
-    'plugin:testing-library/react'
-    // "plugin:@tanstack/eslint-plugin-query/recommended",
+    'plugin:testing-library/react',
+    'plugin:@tanstack/eslint-plugin-query/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
