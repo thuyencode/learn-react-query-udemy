83e6a185b8fdfb7792260f746f7aeb18d4934488
Thuyen Code <thuyencode@proton.me>
2024-07-23 10:09:49 +0700

    Code Quiz! Mutation to Cancel an Appointment

--- base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
+++ base-lazy-days/client/src/components/appointments/hooks/useCancelAppointment.ts
@@ -1,22 +1,36 @@
-import { Appointment } from "@shared/types";
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
-import { axiosInstance } from "@/axiosInstance";
-import { useCustomToast } from "@/components/app/hooks/useCustomToast";
-import { queryKeys } from "@/react-query/constants";
+import { Appointment } from '@shared/types'
 
-// for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+import { axiosInstance } from '@/axiosInstance'
+import { useCustomToast } from '@/components/app/hooks/useCustomToast'
+import { queryKeys } from '@/react-query/constants'
+
+/**
+ * For when server call is needed
+ *
+ * @async
+ * @param {Appointment} appointment
+ * @returns {Promise<void>}
+ */
+async function removeAppointmentUser(appointment: Appointment): Promise<void> {
+  const patchData = [{ op: 'remove', path: '/userId' }]
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData
+  })
+}
 
 export function useCancelAppointment() {
-  const toast = useCustomToast();
+  const toast = useCustomToast()
+  const queryClient = useQueryClient()
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    mutationFn: removeAppointmentUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.appointments] })
+      toast({ title: 'You have cancelled the appointment!', status: 'warning' })
+    }
+  })
+
+  return mutate
 }
